f8849fa4b3ad7a2d1260e554c446e7766105ef09
Jared Bledsoe <jaredbledsoe@MacBook-Air-3.local>
2026-02-01 15:53:38 -0600

    Fix App.jsx and index.html for build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-pest-control/", // MUST match your repo name
+  base: "/ai-pest-control/", // this is ONLY for production build paths
 });
